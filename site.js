9d8372becd184f716ab954fffadc1c38428c817f
Jim <jim-m-kemper@hotmail.com>
2022-09-15 20:48:37 -0600

    Finished initial setup

--- site.js
+++ site.js
@@ -2,10 +2,9 @@ const API_URL = "https://localhost:7092/";
 
 $(document).ready(() => {
   getApiMetadata();
-  addFormSection();
 });
 
-function getApiMetadata() {
+async function getApiMetadata() {
   console.log("Retrieving metadata from " + API_URL + "ApiData");
   $.ajax({
     url: API_URL + "ApiData",
@@ -15,18 +14,21 @@ function getApiMetadata() {
     success: function (data) {
       addRelations(data.relations);
       addQueryTypes(data.queryTypes);
+      addFormSection();
       return data;
     },
   });
 }
 
 function addRelations(data) {
+  console.log("Adding relations");
   $.each(data, function (k, v) {
     $("#relation-dropdown").append(`<option id=${k}>` + v + "</option>");
   });
 }
 
 function addQueryTypes(data) {
+  console.log("Adding QueryTypes");
   $.each(data, function (k, v) {
     $("#query-dropdown").append(`<option id=${k}>` + v + "</option>");
   });
@@ -42,21 +44,51 @@ function addFormSection() {
 
 function getResult() {
   console.log("retrieving...not");
-  $("#submitButton").text("");
-  $("#submitButton").append(
-    "<span class='spinner-border spinner-border-sm' role='status'></span>"
-  );
-  var form = $("#queryForm");
-  console.log(form.serialize());
+  toggleSubmitButton();
+  var request = {
+    queryParts: [],
+  };
+  $("#relation-section")
+    .children()
+    .each(function (k, v) {
+      console.log(v);
+      var relationId = $(v)
+        .find("#relation-dropdown option:selected")
+        .attr("id");
+      // var championId = $(v).find("#relation-dropdown option:selected").attr('id')
+      var championId = $(v).find("#target").val();
+      request.queryParts.push({
+        relation: parseInt(relationId),
+        champion: championId,
+      });
+    });
+  request.queryType = parseInt($("#query-dropdown option:selected").attr("id"));
+  request.summonerName = $("form #summonerName").val();
+  console.log(request);
   $.ajax({
     url: API_URL + "Query",
     type: "POST",
-    data: form.serialize(),
+    contentType: "application/json",
+    data: JSON.stringify(request),
     success: function (data) {
       console.log("YAYAYAYAY");
-      console.log(data);
+      delete data.data.games;
+      $("#results span").text(JSON.stringify(data, null, 2));
+      toggleSubmitButton();
     },
   });
-  $("#results span").text("Not Happening.");
+  // $("#results span").text("Not Happening.");
   return false;
+}
+
+function toggleSubmitButton() {
+  if ($("#submitButton").text() == "") {
+    $("#submitButton").text("Get Results");
+    $("#submitButton").children("span")[0].remove();
+  } else {
+    $("#submitButton").text("");
+    $("#submitButton").append(
+      "<span class='spinner-border spinner-border-sm' role='status'></span>"
+    );
+  }
 }
